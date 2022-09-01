e06d597cff12ce4313bf487185ce769ad55a7f23
indy6678 <indy6678@gmail.com>
2022-08-31 20:05:29 -0500

    updated server.js

--- server.js
+++ server.js
@@ -2,4 +2,24 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({helpers});
 const session = require('express-session');
+const sequelize = require('sequelize');
 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+
+// turn routes on
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false}).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}!`));
+});
