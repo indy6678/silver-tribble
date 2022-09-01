adaa8f5db1a37434e9499e373e56cc8a6789fe63
indy6678 <indy6678@gmail.com>
2022-08-31 20:25:09 -0500

    added api/index

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
+const apiRoutes = require('./api');
 
+router.use('/api', apiRoutes);
+
+
+router.get('/', (req, res) => {
+    res.json({message: 'here we are!'});
+})
+
+// catch all nonexistent routes
 router.use((req, res) => {
-    res.status(404).end();
+    res.status(404).send("<h1>Route not found!</h1><h2>Go back and try again!</h2>").end();
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,22 @@
+const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({helpers});
+// const hbs = exphbs.create({helpers});
 const session = require('express-session');
-const sequelize = require('sequelize');
+const sequelize = require('./config/connection');
+const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session(sess));
+// app.use(session(sess));
 
 // turn routes on
 app.use(routes);
