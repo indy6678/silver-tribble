da1f9f31d99c255da48933dafab5069537895e4b
indy6678 <indy6678@gmail.com>
2022-09-02 11:49:40 -0500

    formatted

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
-const router = require('express').Router();
+const router = require("express").Router();
 // const sequelize = require('../config/connection')
 
 // test route for config validation
@@ -8,57 +8,57 @@ const router = require('express').Router();
 //     res.json({message: 'here we are!'});
 // })
 
-router.get('/', (req, res) => {
-    User.findAll({
-        attributes: {exclude: ['password']},
-        
-    })
+// Get to show all users
+router.get("/", (req, res) => {
+  User.findAll({
+    attributes: { exclude: ["password"] },
+  })
     .then((dbUserData) => {
-        console.log(dbUserData);
-        res.json(dbUserData);
+      // console.log(dbUserData);
+      res.json(dbUserData);
     })
     .catch((err) => {
-    res.status(500).json(err);
+      res.status(500).json(err);
     });
 });
 
-router.get('/:id', (req, res) => {
-    User.findOne({
-        attributes: { exclude: ['password']},
-        where: {
-            id: req.params.id,
-        },
-        include: [
-            {
-                model: User,
-                attributes: ['email', 'username', 'id']
-            },
-        ],
-    })
+// Get to show an individual user by id
+router.get("/:id", (req, res) => {
+  User.findOne({
+    attributes: { exclude: ["password"] },
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: User,
+        attributes: ["id", "email", "username"],
+      },
+    ],
+  })
     .then((dbUserData) => {
-        if(!dbUserData) {
-            res.status(404).json({message: 'No user was found with that id'})
-            return;
-        }
-        res.json(dbUserData);
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user was found with that id" });
+        return;
+      }
+      res.json(dbUserData);
     })
     .catch((err) => {
-        res.status(500).json(err);
-    })
+      res.status(500).json(err);
+    });
 });
 
-router.post('/', (req, res) => {
-    User.create({
-        email: req.body.email,
-        username: req.body.username,
-        password: req.body.password,
-    })
+router.post("/", (req, res) => {
+  User.create({
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password,
+  })
     .then((dbUserData) => res.json(dbUserData))
     .catch((err) => {
-        console.log(err)
-        res.status(500).json(err)
-    })
-})
-
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
