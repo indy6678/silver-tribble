2546e91de55bf9eb14b3c660b7aa9ba1749d4d71
indy6678 <indy6678@gmail.com>
2022-09-02 22:18:36 -0500

    updated home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -8,61 +8,30 @@ const { User, Site } = require("../models");
 //     res.json({message: 'here we are!'});
 // })
 
+// GET to show all websites
+router.get("/", (req, res) => {
+    Site.findAll({
+      attributes: ["id", "website",],
+      include:{
+        model: User,
+        attributes: ['username'],
+      },
+    })
+      .then((dbSiteData) => {
+        console.log(dbSiteData);
+        // serialize the data
+        const sites = dbSiteData.map(site=> site.get({ plain: true}));
+        // passes a single site object into homepage template
+        res.render('homepage', {
+            sites,
+        })
+        // res.json(dbSiteData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).send(err);
+      });
+  });
 
-
-// GET to show an individual user by id
-// router.get("/:id", (req, res) => {
-//   User.findOne({
-//     attributes: { exclude: ["password"] },
-//     where: {
-//       id: req.params.id,
-//     },
-//     attributes: ['id', 'email', 'username'],
-//   })
-//     .then((dbUserData) => {
-//       if (!dbUserData) {
-//         res.status(404).json({ message: "No user was found with that id" });
-//         return;
-//       }
-//       res.json(dbUserData);
-//     })
-//     .catch((err) => {
-//       res.status(500).json(err);
-//     });
-// });
-
-//Post to create a new user
-// router.post("/", (req, res) => {
-//   User.create({
-//     email: req.body.email,
-//     username: req.body.username,
-//     password: req.body.password,
-//   })
-//     .then((dbUserData) => res.json(dbUserData))
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
-
-//DELETE to delete a selected user
-// router.delete('/:id', (req, res) => {
-//     User.destroy({
-//         where: {
-//             id: req.params.id,
-//         },
-//     })
-//     .then((dbUserData) => {
-//         if(!dbUserData) {
-//             res.status(404).json({ message: "No user was found with that id"})
-//             return;
-//         }
-//         res.json(dbUserData);
-//     })
-//     .catch((err)=> {
-//         // console.log(err);
-//         res.status(500).json(err);
-//     })
-// })
 
 module.exports = router;
