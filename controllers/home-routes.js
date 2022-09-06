56ef738b7468d9172d97e88da6449fd24f81e6cf
indy6678 <indy6678@gmail.com>
2022-09-05 21:14:06 -0500

    added session

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { User, Site } = require("../models");
+const { Site } = require("../models");
 
 // const sequelize = require('../config/connection')
 
@@ -21,71 +21,14 @@ router.get("/", (req, res) => {
         // passes a single site object into homepage template
         res.render('homepage', {
             sites,
+            loggedIn: req.session.loggedIn
         })
-        // res.json(dbSiteData);
       })
       .catch((err) => {
         console.log(err);
         res.status(500).send(err);
       });
   });
-
-  
-
-// GET to show an individual user by id
-// router.get("/:id", (req, res) => {
-//   User.findOne({
-//     attributes: { exclude: ["password"] },
-//     where: {
-//       id: req.params.id,
-//     },
-//     attributes: ['id', 'email', 'username'],
-//   })
-//     .then((dbUserData) => {
-//       if (!dbUserData) {
-//         res.status(404).json({ message: "No user was found with that id" });
-//         return;
-//       }
-//       res.json(dbUserData);
-//     })
-//     .catch((err) => {
-//       res.status(500).json(err);
-//     });
-// });
-
-//Post to create a new user
-// router.post("/", (req, res) => {
-//   User.create({
-//     email: req.body.email,
-//     username: req.body.username,
-//     password: req.body.password,
-//   })
-//     .then((dbUserData) => res.json(dbUserData))
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
-
-//DELETE to delete a selected user
-// router.delete('/:id', (req, res) => {
-//     User.destroy({
-//         where: {
-//             id: req.params.id,
-//         },
-//     })
-//     .then((dbUserData) => {
-//         if(!dbUserData) {
-//             res.status(404).json({ message: "No user was found with that id"})
-//             return;
-//         }
-//         res.json(dbUserData);
-//     })
-//     .catch((err)=> {
-//         // console.log(err);
-//         res.status(500).json(err);
-//     })
-// })
 
 // login route
 router.get('/login', (req, res) => {
