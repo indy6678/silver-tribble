bd27fc53503dd67cfbdfa827e4e1076793a19ff1
Your Name <angeltcastillo11@gmail.com>
2022-09-06 20:05:17 -0500

    updated html code

--- controllers/api/notif.js
+++
@@ -1,6 +0,0 @@
-import NotifmeSdk from 'notifme-sdk'
-
-const notifmeSdk = new NotifmeSdk({useNotificationCatcher:}) // empty config = all providers are set to console.log
-notifmeSdk
-  .send({sms: {from: '+15000000000', to: '+15000000001', text: 'Hello, how are you?'}})
-  .then(console.log)
--- db/schema.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS blocktivity_db;
-
-CREATE DATABASE blocktivity_db;
-
-USE DATABASE blocktivity_db;
