e81e69ae2718568cc7600f9252a1d7769fa93d6e
ANGEL CASTILLO JR <ANGELTCASTILLO11@GMAIL.COM>
2023-08-26 14:38:39 -0500

    adding a menu so user can add what url they desire to block,also welcome page is complete

---
+++ background.js
@@ -0,0 +1,14 @@
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === 'block') {
+      const blockedUrl = request.url;
+      chrome.webRequest.onBeforeRequest.addListener(
+        function(details) {
+          return { cancel: true };
+        },
+        { urls: [blockedUrl] },
+        ['blocking']
+      );
+      sendResponse({ message: 'Website blocked successfully' });
+    }
+  });
+  
---
+++ popup.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const blockButton = document.getElementById('blockButton');
+    blockButton.addEventListener('click', blockWebsite);
+  });
+  
+  function blockWebsite() {
+    const websiteUrl = document.getElementById('websiteUrl').value;
+    chrome.runtime.sendMessage({ action: 'block', url: websiteUrl });
+  }
+  
