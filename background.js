ce764fd5e170bb484f9420f69d58f4efb0a2a424
ANGEL CASTILLO JR <ANGELTCASTILLO11@GMAIL.COM>
2023-08-26 18:03:43 -0500

    background.js

--- background.js
+++ background.js
@@ -1,3 +1,37 @@
+// Define the isBlockedUrl function
+function isBlockedUrl(url, callback) {
+  chrome.storage.local.get('blockedUrls', function(result) {
+    const blockedUrls = result.blockedUrls || [];
+
+    // Check if the URL matches any blocked URL in the list
+    const isBlocked = blockedUrls.some(blockedUrl => url.includes(blockedUrl));
+    callback(isBlocked);
+  });
+}
+
+// Add the listener to intercept and block requests
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+    isBlockedUrl(details.url, function(isBlocked) {
+      if (isBlocked) {
+        return { cancel: true };
+      }
+    });
+  },
+  { urls: ["<all_urls>"] },
+  ["blocking"]
+);
+
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+    if (isBlockedUrl(details.url)) {
+      return { cancel: true };
+    }
+  },
+  { urls: ["<all_urls"] },
+  ["blocking"]
+);
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action === 'block') {
     const blockedUrl = request.url;
