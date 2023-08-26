33d49e1545b3ebad0a137ab82f8f9f8463bbb834
ANGEL CASTILLO JR <ANGELTCASTILLO11@GMAIL.COM>
2023-08-26 17:22:55 -0500

    ehu

--- background.js
+++ background.js
@@ -1,32 +1,24 @@
-// background.js
-// This script will run in the background context
-
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action === 'block') {
     const blockedUrl = request.url;
 
-    // Send a message to the content script in the active tab
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      const activeTab = tabs[0];
-      chrome.scripting.executeScript({
-        target: { tabId: activeTab.id },
-        function: blockWebsite,
+    // Store the blocked URL in storage
+    chrome.storage.local.get('blockedUrls', function(result) {
+      const blockedUrls = result.blockedUrls || [];
+      blockedUrls.push(blockedUrl);
+
+      chrome.storage.local.set({ blockedUrls: blockedUrls }, function() {
+        // Update permissions dynamically
+        chrome.permissions.request({ origins: [blockedUrl + '/*'] }, function(granted) {
+          if (granted) {
+            console.log('Permissions Granted:', blockedUrl);
+          } else {
+            console.log('Permissions Denied', blockedUrl);
+          }
+        });
+
+        sendResponse({ message: 'URL blocked successfully' });
       });
     });
-
-    sendResponse({ message: 'Website block request sent' });
   }
 });
-
-
-function blockWebsite(blockedUrl) {
-  // Get the current URL of the webpage
-  const currentUrl = window.location.href;
-
-  // Check if the current URL matches the blocked URL
-  if (currentUrl.includes(blockedUrl)) {
-    // Redirect the user to a different page (e.g., a blocked page)
-    window.location.href = 'https://example.com/blocked-page';
-  }
-}
-
--- popup.js
+++ popup.js
@@ -1,9 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
+  const urlInput = document.getElementById('urlInput');
   const blockButton = document.getElementById('blockButton');
-  blockButton.addEventListener('click', blockWebsite);
+
+  blockButton.addEventListener('click', function() {
+    const url = urlInput.value.trim();
+    if (isValidUrl(url)) {
+      chrome.runtime.sendMessage({ action: 'block', url: url }, function(response) {
+        console.log(response.message);
+      });
+    } else {
+      console.log('Invalid URL');
+    }
+  });
 });
 
-function blockWebsite() {
-  const websiteUrl = document.getElementById('websiteUrl').value;
-  chrome.runtime.sendMessage({ action: 'block', url: websiteUrl });
+function isValidUrl(url) {
+  // Add your URL validation logic here
+  return url.startsWith('http://') || url.startsWith('https://');
 }
